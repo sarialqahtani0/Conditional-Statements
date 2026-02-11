b74b0b1efe50511f1d3b8dbe7c7ee61e5970f2ea
sarialqahtani0 <sarialqahtani527@gmail.com>
2026-02-11 17:59:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-public class Main {
+public class VowelOrConsonant {
     public static void main(String[] args) {
-      /* add your code here */
-    }
 
+        char value = 'a';
+
+        if (value >= 'a' && value <= 'z') {
+
+            if (value == 'a' || value == 'e' || value == 'i' || value == 'o' || value == 'u') {
+                System.out.println("It is a vowel.");
+            } else {
+                System.out.println("It is a consonant.");
+            }
+
+        } else {
+            System.out.println("Not an alphabet character.");
+        }
+    }
 }
